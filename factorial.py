e54e964b425179d30ded5e57a4166b63dbb257fa
csuman243 <84701685+csuman243@users.noreply.github.com>
2021-11-23 13:45:33 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,13 @@
+
+num=int(input("Enter your number here : "))
+
+factorial=1
+
+if num<0:
+    print("Enter positive integer number")
+elif num==0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        factorial=factorial*i
+        print("The factorial is",factorial)
